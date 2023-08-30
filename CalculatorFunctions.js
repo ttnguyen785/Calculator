d68227802f10528843f611fb826c5fa1c8fac48b
David <david.nguyen00@outlook.com>
2023-08-30 14:52:53 -0700

    All keys functions are intented

--- CalculatorFunctions.js
+++ CalculatorFunctions.js
@@ -1,35 +1,9 @@
-function add(num1, num2)  {
-  return num1 + num2;
-	
-};
- function subtract(num1, num2) {
-  return num1 - num2;
-	
-};
-
-function multiply (num1, num2) {
-    return num1 * num2;
-}
-
-function divide (num1, num2) {
-    return num1 / num2;
-}
-
-function operatate(operator, num1, num2) {
-    if (operator == "+") {
-        return add(num1, num2);
-    }
-}
-
-
 
 const display = document.getElementById("display");
-const buttons = document.querySelectorAll("#keys button:not(#acButton)");
+const buttons = document.querySelectorAll("#keys button:not(#acButton):not(#equal-key)");
 const acButton = document.getElementById("acButton");
 
-
 let isFirstClick = true;
-let currentNumber = '';
 
 acButton.addEventListener("click", () => {
   display.textContent = "0";
@@ -40,7 +14,6 @@ buttons.forEach(button => {
   button.addEventListener("click", () => {
     const number = button.textContent;
     const currentValue = display.textContent;
-
     display.textContent = currentValue === "0" ? number : currentValue + number;
   });
 });
